f2568bf1f99f70a06f573930ea1ff2bc08a1cb08
Michael Creel <michael.creel@uab.es>
2017-08-22 00:35:26 +0200

    remove octave support code

--- MyOctaveFiles/Econometrics/Kernel/kernel_regression_cvscore.m
+++
@@ -1,9 +0,0 @@
-function cvscore = kernel_regression_cvscore(bandwidth, data, depvar, kernel, prewhiten)
-	if (nargin < 4) kernel = "__kernel_normal"; endif
-	if (nargin < 5) prewhiten = false; endif
-	
-	fit = kernel_regression(data, depvar, data, exp(bandwidth), kernel, prewhiten, true);
-	cvscore = depvar - fit;
-	cvscore = cvscore'*cvscore;
-endfunction
-
--- MyOctaveFiles/Econometrics/LinearRegression/rsquare.m
+++
@@ -1,9 +0,0 @@
-# R^2 from linear regression
-function rsq = rsquare(y, x)
-	[b, sigsq, e] = ols(y,x);
-
-	tss = y - mean(y);
-	tss = tss' * tss;
-	ess = e' * e;
-	rsq = 1 - ess / tss;
-endfunction
