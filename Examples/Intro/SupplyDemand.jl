674f91d36453351e2b27b44ac4a8ebd240927cdc
Michael Creel <michael.creel@uab.es>
2018-03-18 15:59:15 +0100

    minor

--- Examples/Intro/SupplyDemand.jl
+++ Examples/Intro/SupplyDemand.jl
@@ -1,10 +1,10 @@
 # this generates data from the simple supply-demand model
-# q = a1 + a2*p + a3*m + e1
-# q = b1 + b2*p +        e2
+# q = α1 + α2*p + α3*m + e1
+# q = β1 + β2*p +        e2
 
 function main()
 # number of obsn
-n = 1000
+n = 500
 # model parameters
 α1 = 100.0
 α2 = -1.0
@@ -15,8 +15,8 @@ n = 1000
 m = randn(n,5)
 m = sum(m.*m,2) # chi square df=5
 # structural shocks
-ϵ1 = 2.0*randn(n)
-ϵ2 = 2.0*randn(n)
+ϵ1 = 1.0*randn(n)
+ϵ2 = 1.0*randn(n)
 # rf shocks
 ν1 = (β2*ϵ1-α2*ϵ2)/(β2-α2)
 ν2 = (ϵ1-ϵ2)/(β2-α2)
@@ -25,10 +25,17 @@ m = sum(m.*m,2) # chi square df=5
 π21 = β2*α3/(β2-α2)
 π12 = (α1-β1)/(β2-α2)
 π22 = α3/(β2-α2)
-# sf variables
+# sf variables computed from rf
 q = π11 + π21*m + ν1
 p = π12 + π22*m + ν2
 data = [q p m]
+data = sortbyc(data,3)
+nn = Int(n/4)
+scatter(data[1:nn,2], data[1:nn,1], markersize=8, xlabel="price", ylabel="quantity", legend=false)
+for i = 2:4
+scatter!(data[(i-1)*nn+1:i*nn,2], data[(i-1)*nn:i*nn,1], markersize=8)
+end
+
 writedlm("data.txt", data)
 end
 main()
