72a0b6780234af598a7a060d1b0a59e411f38959
Michael Creel <michael.creel@uab.es>
2018-04-23 14:03:02 +0200

    fix OLSvsIV.jl

--- Examples/GMM/Hausman/OLSvsIV.jl
+++ Examples/GMM/Hausman/OLSvsIV.jl
@@ -9,25 +9,24 @@ betaiv = zeros(reps,2)
 n = 1000 # sample size
 
 # covariance of X, W, e
-cov_X_W = 1.0  # experiment with lowering or raising this: quality of instrument
+cov_X_W = 0.2  # experiment with lowering or raising this: quality of instrument
 cov_X_e = 0.5
 sig = [
-      3.0       cov_X_W     cov_X_e;
+      1.0       cov_X_W     cov_X_e;
       cov_X_W   1.0         0.0;
-     cov_X_e    0.0         1.0];
-
+     cov_X_e    0.0         1.0]
 truebeta = [1, 2] # true beta
-p = chol(sig);
+p = chol(sig)
 for i = 1:reps
 	XWE = randn(n,3)*p
-	e = XWE[:,3]
-	w = [ones(n,1)  XWE[:,2]]
-	x = [ones(n,1) XWE[:,1]]
+	e = XWE[:,3:3]
+	w = [ones(n) XWE[:,2:2]]
+	x = [ones(n) XWE[:,1:1]]
 	y = x*truebeta + e
 	# OLS
-	betaols[i,:] = y\x
+	betaols[i,:] = (x\y)'
 	# IV
-	betaiv[i,:] = inv(w'*x)*w'*y
+	betaiv[i,:] = (inv(w'*x)*w'*y)'
 end
 
 histogram(betaols[:,2], nbins=50)
